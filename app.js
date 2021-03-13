5b4dd84f4b220a89c0bbb0e883cbf1deb8edc6f1
Matthew Smart <matthewsmart13@gmail.com>
2021-03-13 02:44:29 -0600

    Full game script working

--- app.js
+++ app.js
@@ -4,7 +4,50 @@ const scissorsButton = document.querySelector('.scissors');
 let playerScore = 0;
 let computerScore = 0;
 
+function socreUpdater (playerScore, computerScore) {
+    const player = document.querySelector('#player-score');
+    player.innerHTML = playerScore.toString();
+    const computer = document.querySelector('#computer-score');
+    computer.innerHTML = computerScore.toString();
+}
 
+function gameLog (winner, playerSelection, computerSelection) {
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    if (winner === "player") {
+        li.textContent = "You win! " + playerSelection + " beats " + computerSelection;
+    } else if (winner === "computer") {
+        li.textContent = "You lose. " + computerSelection + " beats " + playerSelection;
+    } else {
+        li.textContent = "Tie game. No winner";
+    }
+    ul.appendChild(li)
+}
+
+function gameOver(playerScore, computerScore) {
+    if (playerScore === 5) {
+        winnerMessage(playerScore);
+        removeEvent()
+        return true;
+    } else if (computerScore === 5) {
+        winnerMessage(playerScore);
+        removeEvent();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function winnerMessage (playerScore){
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    if (playerScore === 5) {
+        li.textContent = "You win! Press F5 to play again";
+    } else {
+        li.textContent = "You Lost. Press F5 to play again";
+    }
+    ul.appendChild(li);
+}
 
 function computerPlay () {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -20,28 +63,35 @@ function computerPlay () {
 function playGame () {
    const playerSelection = this.dataset.button;
    const computerSelection = computerPlay();
-   console.log("Player: " + playerSelection + "  " + "computer: " + computerSelection);
-
    if ((playerSelection === "rock" && computerSelection === "scissors") || 
         (playerSelection === "paper" && computerSelection === "rock") || 
         (playerSelection === "scissors" && computerSelection === "paper")) 
         {
-            console.log("You win! " + playerSelection + " beats " + computerSelection);
             playerScore++;
-    } else if ((playerSelection === "rock" && computerSelection === "paper") ||
+            const winner = "player";
+            gameLog(winner, playerSelection, computerSelection);
+            socreUpdater(playerScore, computerScore); 
+            gameOver(playerScore, computerScore);
+        } else if ((playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissors") || 
         (playerSelection === "scissors" && computerSelection === "rock")) 
         {
-            console.log("You lose. " + computerSelection + " beats " + playerSelection);
             computerScore++;
-            
-    } else {
-        console.log("Tie game. No winner")
-        return;
-    }
+            const winner = "computer";
+            gameLog(winner, playerSelection, computerSelection);
+            socreUpdater(playerScore, computerScore); 
+            gameOver(playerScore, computerScore);    
+        } else {
+        const winner = "tie";
+        gameLog(winner, playerSelection, computerSelection);
+    } 
 }
 
-
+function removeEvent () {
+    rockButton.removeEventListener('click', playGame);
+    paperButton.removeEventListener('click', playGame);
+    scissorsButton.removeEventListener('click', playGame);
+}
 
 rockButton.addEventListener('click', playGame);
 paperButton.addEventListener('click', playGame);
